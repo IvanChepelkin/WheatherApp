eaf3bb94b8b7eaca0af56e3a5959483472724f77
SpartakChampion <ivan_chepelkin@mail.ru>
2018-08-13 01:55:58 +0300

    Дз №2 Adroid 1 Доработайте погодное приложение: вставьте в него выпадающий список с городами, добавьте кнопку «Узнать погоду» и выводите прогноз по выбранному городу. 2. В файле values/strings.xml​ переведите все строки на английский язык. Русский перевод поместите в файл values-ru/strings.xml​. Проверьте, что при смене языка на устройстве соответственно меняется и интерфейс приложения. 3. * Фиксируйте в памяти последний город, который выбирал пользователь (предполагается использование SharedPreferences). Выводите именно его при повторном запуске приложения

--- app/src/main/java/com/example/ivanchepelkin/wheatherapp/MainActivity.java
+++ app/src/main/java/com/example/ivanchepelkin/wheatherapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ivanchepelkin.wheatherapp;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,15 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // Объявляем наши вью
     private TextView getWeather;
     private Spinner setCity;
     private Button button;
+    private SharedPreferences shareP;
+    final String SAVED_TEXT = "saved_text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         initSpiner();
         setResultBtnClick();
+        loadText();  //загружаем последний выбор спинера
+
 
 
     }
@@ -58,9 +64,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 String text = CitySpec.getCity(MainActivity.this,setCity.getSelectedItemPosition());
                 getWeather.setText(text);
+                saveText(setCity.getSelectedItemPosition()); //отправляем на сохранение позицию
             }
 
 
         });
    }
+   // сохранение данных
+    void saveText(int position) {
+        //делаем позицию типом String, иначе метод putString не воспринимает
+        String pos = String.valueOf(position);
+        //Константа MODE_PRIVATE используется для настройки доступа и означает,
+        // что после сохранения, данные будут видны только этому приложению
+        shareP = getPreferences(MODE_PRIVATE);
+        //чтобы редактировать данные, необходим объект Editor
+        SharedPreferences.Editor ed = shareP.edit();
+        //В метод putString указываем наименование переменной – это константа SAVED_TEXT,
+        // и значение – содержимое поля getWeather
+        ed.putString(SAVED_TEXT, pos);
+        //сохраняем данныеZ
+        ed.commit();
+        //выводи сообщение "Text saved"
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+    void loadText() {
+        shareP = getPreferences(MODE_PRIVATE);
+        int pos;
+        //Читаем с помощью метода getString – в параметрах указываем константу
+        // - это имя, и значение по умолчанию (пустая строка)
+        String savedPos = shareP.getString(SAVED_TEXT, "");
+        //переделываем сохраненную pos обратно в int
+        pos = Integer.parseInt(savedPos);
+        //задаем ее в спинере
+        setCity.setSelection(pos);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+    }
 }
