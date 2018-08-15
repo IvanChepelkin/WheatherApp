d18cc46d6f53a64cba061e2512a3581c3fbbd2d5
SpartakChampion <ivan_chepelkin@mail.ru>
2018-08-15 22:52:45 +0300

    Дз №2 Adroid 1 Исправил эти ошибки:
    6 API ставить НЕЛЬЗЯ - говорил об этом на первом уроке. Гугл запретил аппы на 16 АПИ в орео. Ставим от 17 до 19 в минимальной версии.
    http://prntscr.com/kijit8 http://prntscr.com/kijj4g - избегайте лишних проблелов и закомментированного кода при сдаче.
    В целом код выглядит неплохо!
    
    К сожалению, при старте у вас крэш - http://prntscr.com/kijkab -pos = Integer.parseInt(savedPos); - берете из шэред префернсес значение выше, его нет при первом старте, вы почему-то если ничего нет в качестве дефолта передаете пустую строку - либо проверйте потом, что строка не пуста (предпочтительнее), либо передавайте какое-то дефолтное число в строке.
    
    Язык в эмуляторе можно изменить во встроенном приложении Custom Locale http://prntscr.com/kiju67 http://prntscr.com/kijugx

--- app/src/main/java/com/example/ivanchepelkin/wheatherapp/CitySpec.java
+++ app/src/main/java/com/example/ivanchepelkin/wheatherapp/CitySpec.java
@@ -5,7 +5,9 @@ import android.content.Context;
 public class CitySpec {
     //context получаем доступ к стоковым ресурсам
     static String getCity (Context context, int position   ){
+        //вносим в массив список погоды на наши города
         String [] setWeather = context.getResources().getStringArray(R.array.weather);
+        //отправяем погоду относительно позиции спиннера
         return setWeather[position];
 
     }
--- app/src/main/java/com/example/ivanchepelkin/wheatherapp/MainActivity.java
+++ app/src/main/java/com/example/ivanchepelkin/wheatherapp/MainActivity.java
@@ -25,52 +25,29 @@ public class MainActivity extends AppCompatActivity {
         //указываем, какой макет отображать
         setContentView(R.layout.activity_main);
         initViews();
-        initSpiner();
         setResultBtnClick();
         loadText();  //загружаем последний выбор спинера
-
-
-
     }
-   // метод инициаизирует вьюшки через id
-   private void initViews(){
+
+    // метод инициаизирует вьюшки через id
+    private void initViews() {
         getWeather = findViewById(R.id.result);
         setCity = findViewById(R.id.spinnerForCities);
         button = findViewById(R.id.button_show_weather);
-   }
-   //инициализируем спинер
+    }
 
-   private void initSpiner(){
-        //final String [] cities = getResources().getStringArray(R.array.cityGroup); //в массив вносим города
-       // из массива, что в ресурасах
-       // метод задает город из выпадающего списка и выдает погоду
-//       setCity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//           @Override
-//           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//               String text = CitySpec.getCity(MainActivity.this,position); // получаем из класса CitySpec нашу погоду
-//               getWeather.setText(text); // передаем ее на текстВью
-//           }
-//
-//           @Override
-//           public void onNothingSelected(AdapterView<?> parent) {
-//
-//           }
-//       });
-
-   }
-   private void setResultBtnClick(){
+    private void setResultBtnClick() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                String text = CitySpec.getCity(MainActivity.this,setCity.getSelectedItemPosition());
+            public void onClick(View v) {
+                String text = CitySpec.getCity(MainActivity.this, setCity.getSelectedItemPosition());
                 getWeather.setText(text);
                 saveText(setCity.getSelectedItemPosition()); //отправляем на сохранение позицию
             }
-
-
         });
-   }
-   // сохранение данных
+        }
+
+    // сохранение данных
     void saveText(int position) {
         //делаем позицию типом String, иначе метод putString не воспринимает
         String pos = String.valueOf(position);
@@ -80,13 +57,15 @@ public class MainActivity extends AppCompatActivity {
         //чтобы редактировать данные, необходим объект Editor
         SharedPreferences.Editor ed = shareP.edit();
         //В метод putString указываем наименование переменной – это константа SAVED_TEXT,
-        // и значение – содержимое поля getWeather
+        // и значение – последняя позиция спинера
         ed.putString(SAVED_TEXT, pos);
         //сохраняем данныеZ
         ed.commit();
-        //выводи сообщение "Text saved"
+        //выводим сообщение "Text saved"
         Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
     }
+
+    //вывод сохраненных данных
     void loadText() {
         shareP = getPreferences(MODE_PRIVATE);
         int pos;
@@ -94,9 +73,12 @@ public class MainActivity extends AppCompatActivity {
         // - это имя, и значение по умолчанию (пустая строка)
         String savedPos = shareP.getString(SAVED_TEXT, "");
         //переделываем сохраненную pos обратно в int
-        pos = Integer.parseInt(savedPos);
-        //задаем ее в спинере
-        setCity.setSelection(pos);
-        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+        if(savedPos != "") {
+            pos = Integer.parseInt(savedPos);
+            //задаем ее в спинере
+            setCity.setSelection(pos);
+            //выводим сообщение "Text loaded"
+            Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+        }
     }
 }
